8d307b6f7b186c68742cdc5ff32db6461b5d56d8
Eigur1 <contactealejandro@gmail.com>
2023-12-13 12:31:44 +0100

    Day 30
    Machine migraton + pw manager upgrade

--- Day 26/main.py
+++ Day 26/main.py
@@ -1,12 +1,20 @@
 import pandas as pd
 
 nato_df = pd.read_csv("nato_phonetic_alphabet.csv")
-nato_dic = {nato_df.iloc[index]["letter"]:nato_df.iloc[index]["code"] for (index, row) in nato_df.iterrows()}
+nato_dic = {nato_df.iloc[index]["letter"]: nato_df.iloc[index]["code"] for (index, row) in nato_df.iterrows()}
 
-word = input("What do you wanna natoinize?")
-output = [nato_dic[n.upper()] for n in list(word) if n != " "]
-print(output)
-#Loop through rows of a data frame
+def translate():
+    output = 1
+    word = input("What do you wanna natoinize?")
+    try:
+        output = [nato_dic[n.upper()] for n in list(word) if n != " "]
+    except KeyError:
+        print("Only letters, bish.")
+        translate()
+    if output != 1:
+        print(output)
+translate()
+# Loop through rows of a data frame
 # for (index, row) in student_data_frame.iterrows():
 #     #Access index and row
 #     #Access row.student or row.score
@@ -15,8 +23,7 @@ print(output)
 # Keyword Method with iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
 
-#TODO 1. Create a dictionary in this format:
+# TODO 1. Create a dictionary in this format:
 {"A": "Alfa", "B": "Bravo"}
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-
+# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
